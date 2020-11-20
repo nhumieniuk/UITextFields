976711c65ae2b940dd117725704fc01bdedd0b6b
Natan Humieniuk <nhumieniuk@bsd220.org>
2020-11-20 02:21:21 -0600

    Initial Commit

---
+++ UITextFields/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITextFields
+//
+//  Created by Student on 11/20/20.
+//  Copyright © 2020 elenator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
